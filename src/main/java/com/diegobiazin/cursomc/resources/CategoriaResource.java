427b8c9f464a387d74dc5a46bd27fdb50f8e3d5f
Diego Biazin <diego_biazin@hotmail.com>
2018-07-22 20:52:39 -0300

    Criada classe de domínio Categoria

--- src/main/java/com/diegobiazin/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/diegobiazin/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,23 @@
 package com.diegobiazin.cursomc.resources;
 
+import com.diegobiazin.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar(){
-        return "REST está funcionando!";
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> categorias= Arrays.asList(cat1, cat2);
+        return categorias;
     }
 }
