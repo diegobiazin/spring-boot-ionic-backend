0c36d94ec2b619dd9d3277dd9170bc84feb74529
Diego Biazin <diego_biazin@hotmail.com>
2018-07-26 00:42:53 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/diegobiazin/cursomc/CursomcApplication.java
+++ src/main/java/com/diegobiazin/cursomc/CursomcApplication.java
@@ -1,6 +1,7 @@
 package com.diegobiazin.cursomc;
 
 import com.diegobiazin.cursomc.domain.*;
+import com.diegobiazin.cursomc.domain.enums.EstadoPagamento;
 import com.diegobiazin.cursomc.domain.enums.TipoCliente;
 import com.diegobiazin.cursomc.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.text.SimpleDateFormat;
+
+import static java.util.Arrays.asList;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -26,6 +28,10 @@ public class CursomcApplication implements CommandLineRunner {
     private EnderecoRepository enderecoRepository;
     @Autowired
     private ClienteRepository clienteRepository;
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
@@ -41,15 +47,15 @@ public class CursomcApplication implements CommandLineRunner {
         Produto p2 = new Produto(null, "Impressora", 800.00);
         Produto p3 = new Produto(null, "Mouse", 80.00);
 
-        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-        cat2.getProdutos().addAll(Arrays.asList(p2));
+        cat1.getProdutos().addAll(asList(p1, p2, p3));
+        cat2.getProdutos().addAll(asList(p2));
 
-        p1.getCategorias().addAll(Arrays.asList(cat1));
-        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-        p3.getCategorias().addAll(Arrays.asList(cat1));
+        p1.getCategorias().addAll(asList(cat1));
+        p2.getCategorias().addAll(asList(cat1, cat2));
+        p3.getCategorias().addAll(asList(cat1));
 
-        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+        categoriaRepository.saveAll(asList(cat1, cat2));
+        produtoRepository.saveAll(asList(p1, p2, p3));
 
         Estado est1 = new Estado(null, "Minas Gerais");
         Estado est2 = new Estado(null, "São Paulo");
@@ -58,21 +64,36 @@ public class CursomcApplication implements CommandLineRunner {
         Cidade c2 = new Cidade(null, "São Paulo", est2);
         Cidade c3 = new Cidade(null, "Campinas", est2);
 
-        est1.getCidades().addAll(Arrays.asList(c1));
-        est2.getCidades().addAll(Arrays.asList(c2, c3));
+        est1.getCidades().addAll(asList(c1));
+        est2.getCidades().addAll(asList(c2, c3));
 
-        estadoRepository.saveAll(Arrays.asList(est1, est2));
-        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+        estadoRepository.saveAll(asList(est1, est2));
+        cidadeRepository.saveAll(asList(c1, c2, c3));
 
         Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
-        cli1.getTelefones().addAll(Arrays.asList("27363323", "93837393"));
+        cli1.getTelefones().addAll(asList("27363323", "93837393"));
 
         Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 10", "Jardim", "38220834", cli1, c1);
         Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 08", "Centro", "3877012", cli1, c2);
 
-        cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+        cli1.getEnderecos().addAll(asList(e1, e2));
 
-        clienteRepository.saveAll(Arrays.asList(cli1));
-        enderecoRepository.saveAll(Arrays.asList(e1, e2));
+        clienteRepository.saveAll(asList(cli1));
+        enderecoRepository.saveAll(asList(e1, e2));
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+        Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+        Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+        ped1.setPagamento(pagto1);
+        Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+        ped2.setPagamento(pagto2);
+
+        cli1.getPedidos().addAll(asList(ped1, ped2));
+
+        pedidoRepository.saveAll(asList(ped1, ped2));
+        pagamentoRepository.saveAll(asList(pagto1, pagto2));
+
     }
 }
---
+++ src/main/java/com/diegobiazin/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.diegobiazin.cursomc.repositories;
+
+import com.diegobiazin.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
