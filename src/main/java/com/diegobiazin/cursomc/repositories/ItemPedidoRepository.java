e41fb9b7f462c2e6e1a38d4924ea440caa2489cc
Diego Biazin <diego_biazin@hotmail.com>
2018-07-26 22:06:32 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/diegobiazin/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.diegobiazin.cursomc.repositories;
+
+import com.diegobiazin.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
