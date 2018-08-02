714c184457a5ebbb243c43c8ea9665c9abf19c58
Diego Biazin <diego_biazin@hotmail.com>
2018-08-01 22:50:42 -0300

    Criando o profile de teste

---
+++ src/main/java/com/diegobiazin/cursomc/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.diegobiazin.cursomc.config;
+
+import com.diegobiazin.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
