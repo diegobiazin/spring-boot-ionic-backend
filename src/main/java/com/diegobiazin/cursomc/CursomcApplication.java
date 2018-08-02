3b9b883f06e3a9fc0e7582f53a0d320b4a9d7267
Diego Biazin <diego_biazin@hotmail.com>
2018-08-01 23:04:37 -0300

    Criando o profile de desenvolvimento

--- src/main/java/com/diegobiazin/cursomc/CursomcApplication.java
+++ src/main/java/com/diegobiazin/cursomc/CursomcApplication.java
@@ -1,17 +1,8 @@
 package com.diegobiazin.cursomc;
 
-import com.diegobiazin.cursomc.domain.*;
-import com.diegobiazin.cursomc.domain.enums.EstadoPagamento;
-import com.diegobiazin.cursomc.domain.enums.TipoCliente;
-import com.diegobiazin.cursomc.repositories.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.text.SimpleDateFormat;
-
-import static java.util.Arrays.asList;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
