78a1bd6683be50e5a881c80f3188967ef3d8fadb
Diego Biazin <diego_biazin@hotmail.com>
2018-08-08 23:02:05 -0300

    Salvando URL da imagem em Cliente

--- src/main/java/com/diegobiazin/cursomc/services/ClienteService.java
+++ src/main/java/com/diegobiazin/cursomc/services/ClienteService.java
@@ -45,7 +45,7 @@ public class ClienteService {
     public Cliente find(Integer id) {
 
         UserSS user = UserService.authenticated();
-        if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+        if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
             throw new AuthorizationException("Acesso negado");
         }
 
@@ -111,6 +111,16 @@ public class ClienteService {
     }
 
     public URI uploadProfilePicture(MultipartFile multipartFile) {
-        return s3Service.uploadFile(multipartFile);
+        UserSS user = UserService.authenticated();
+        if (user == null)
+            throw new AuthorizationException("Acesso negado!");
+
+        URI uri = s3Service.uploadFile(multipartFile);
+        Cliente cliente = find(user.getId());
+        cliente.setImageUrl(uri.toString());
+
+        clienteRepository.save(cliente);
+
+        return uri;
     }
 }
