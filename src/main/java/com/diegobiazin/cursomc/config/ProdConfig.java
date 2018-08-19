e57bc18997bb53797ec5bd70731a837b012ad00c
Diego Biazin <diego_biazin@hotmail.com>
2018-08-19 15:52:35 -0300

    Alterações ProdConfig

--- src/main/java/com/diegobiazin/cursomc/config/ProdConfig.java
+++ src/main/java/com/diegobiazin/cursomc/config/ProdConfig.java
@@ -1,5 +1,10 @@
 package com.diegobiazin.cursomc.config;
 
+import com.diegobiazin.cursomc.services.DBService;
+import com.diegobiazin.cursomc.services.EmailService;
+import com.diegobiazin.cursomc.services.SmtpEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,8 +15,23 @@ import java.text.ParseException;
 @Profile("prod")
 public class ProdConfig {
 
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
     @Bean
-    public boolean instantiateDataBase() throws ParseException {
+    public boolean instantiateDatabase() throws ParseException {
+        if (!"create".equals(strategy))
+            return false;
+
+        dbService.instantiateTestDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService emailService() {
+        return new SmtpEmailService();
     }
 }
